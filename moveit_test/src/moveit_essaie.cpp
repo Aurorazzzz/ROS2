c5c447672aafbc4c7f671e7fad66f24d51fe1e04
Aurore <aurore.bbinder@gmail.com>
2025-12-14 15:08:19 +0100

    pose

--- moveit_test/src/moveit_essaie.cpp
+++ moveit_test/src/moveit_essaie.cpp
@@ -67,8 +67,8 @@ int main(int argc, char * argv[])
   p1.orientation.y = 0.147;
   p1.orientation.z = -0.140;
   p1.orientation.w = 0.709;   // orientation identité
-  p1.position.x = -0.487;
-  p1.position.y = 0.109;
+  p1.position.x = -0.07;
+  p1.position.y = 0.759;
   p1.position.z = 0.433;
 
   // -----------------------------
