cc7500ceae4905d23bf4fb25a08eca56497dedeb
Aurore <aurore.bbinder@gmail.com>
2025-12-14 14:31:07 +0100

    quaternion

--- moveit_test/src/moveit_essaie.cpp
+++ moveit_test/src/moveit_essaie.cpp
@@ -66,7 +66,7 @@ int main(int argc, char * argv[])
   p1.orientation.x = 0.707;
   p1.orientation.y = 0.707;
   p1.orientation.z = 0.0;
-  p1.orientation.w = 1.0;   // orientation identité
+  p1.orientation.w = 0.0;   // orientation identité
   p1.position.x = -0.487;
   p1.position.y = 0.109;
   p1.position.z = 0.433;
