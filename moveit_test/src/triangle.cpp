e6f3c317190ffe172052cb60a4492ef446c1b30e
Aurore <aurore.bbinder@gmail.com>
2025-12-14 18:58:55 +0100

    orien

--- moveit_test/src/triangle.cpp
+++ moveit_test/src/triangle.cpp
@@ -166,8 +166,8 @@ try {
 
   for (size_t i = 0; i < (points2d.size()); ++i) {
     geometry_msgs::msg::Pose p = start_pose;
-    p.position.y +=  points2d[i].u;
-    p.position.z +=  points2d[i].v;
+    p.position.x +=  points2d[i].u;
+    p.position.y +=  points2d[i].v;
     RCLCPP_INFO(node->get_logger(), "[%zu] u=%.4f v=%.4f pen=%d",
                 i, points2d[i].u, points2d[i].v, points2d[i].pen ? 1 : 0);
      waypoints.push_back(p); 
