28d96e7f145fc9a19562b1de3732cd1398f5857f
Aurore <aurore.bbinder@gmail.com>
2026-01-06 20:08:36 +0100

    petir

--- moveit_test/data/generation_data.py
+++ moveit_test/data/generation_data.py
@@ -17,8 +17,8 @@ CENTER_V = 0.00
 # Taille du dessin (dans VOS unités)
 # -> le dessin sera contenu dans un carré/rectangle de cette taille en gardant le ratio
 # Exemple : 0.20 = 20 cm si vos unités sont en mètres
-DRAW_WIDTH  = 0.50
-DRAW_HEIGHT = 0.50
+DRAW_WIDTH  = 0.30
+DRAW_HEIGHT = 0.30
 
 # Échantillonnage : espacement entre points successifs (dans VOS unités)
 POINT_SPACING = 0.005  # 2 mm si unités = mètres
