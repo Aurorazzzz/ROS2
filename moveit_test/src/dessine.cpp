26ce0fadbcc925a4ef25db60264d19cf68abfb3d
Aurore <aurore.bbinder@gmail.com>
2026-01-06 19:03:25 +0100

    delay

--- moveit_test/src/dessine.cpp
+++ moveit_test/src/dessine.cpp
@@ -149,7 +149,8 @@ try {
   return 1;
 }
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+RCLCPP_INFO(node->get_logger(), "Dodo");
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
   // definir le départ du prochain plan = position actuel
   move_group_interface.setStartStateToCurrentState();
 
